59832d47e41b1558507fc2c1c29b8d2455d74da4
Alexander Steffen <alex@ajhs.io>
2024-02-13 20:57:45 -0800

    Updated to the latest upstream.

---
+++ src/DocsAfterDark/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Alexander Jonathan Hardy Steffen on 2024-02-13.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
